82a6b67835a4d023e9f0319cfa453cabb941c503
Shunpoco <tkngsnsk313320@gmail.com>
2022-04-19 20:45:38 +0900

    Login

--- client/main.go
+++ client/main.go
@@ -8,14 +8,30 @@ import (
 	"net/http"
 )
 
+type Env struct {
+	Login        string
+	Token        string
+	UserInfo     string
+	ClientId     string
+	ClientSecret string
+}
+
 func main() {
 	code, err := ioutil.ReadFile("./template/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 	t := string(code)
+	base := "http://localhost:8080/realms/test/protocol/openid-connect"
+	env := Env{
+		Login:        fmt.Sprintf("%s/auth", base),
+		Token:        fmt.Sprintf("%s/token", base),
+		UserInfo:     fmt.Sprintf("%s/userinfo", base),
+		ClientId:     "",
+		ClientSecret: "",
+	}
 
-	http.HandleFunc("/", mainHandler(t))
+	http.HandleFunc("/", mainHandler(t, &env))
 	http.HandleFunc("/redirect", redirectHandler)
 	http.HandleFunc("/logged-in", loggedInHandler)
 
@@ -24,13 +40,13 @@ func main() {
 	}
 }
 
-func mainHandler(t string) func(w http.ResponseWriter, r *http.Request) {
+func mainHandler(t string, env *Env) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.New("template").Parse(t)
 		if err != nil {
 			fmt.Fprintf(w, "404")
 		}
-		tmpl.Execute(w, nil)
+		tmpl.Execute(w, env)
 	}
 }
 
